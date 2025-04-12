25d06291e93ee0ff5e78ca885944f383edd1a3ba
Sahil19 <sahilkulria9988@gmail.com>
2025-04-12 16:34:47 +0530

    1st step

--- patterns.java
+++ patterns.java
@@ -7,7 +7,7 @@ public class patterns {
 
 
 
-    }
+    }}
   /*     type 1 (small to big *)
     static void pattern1(int n) {
         for (int row = 1; row <= n; row++) {
@@ -86,7 +86,7 @@ public class patterns {
             }
             System.out.println();
         }
-    } */
+    } 
 
     // type 4 numbering type
     static void pattern1(int n) {
@@ -101,3 +101,122 @@ public class patterns {
 
 
 }
+ 
+
+ // Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+  -----------------------------// DIAMOND PATTERN UPPER/LOWER ---------------------------
+
+
+class Main {
+    public static void main(String[] args) {
+         int n = 4;
+        for (int i=1; i<=n; i++) {
+            for (int j=1 ; j<=n-i ; j++){   //n-1 space hai 
+                System.out.print(" ");
+            }
+            for (int j=1; j<=(2*i-1); j++){        //2*i-1 star print hore
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+        
+        for (int i = n; i>=1; i--){
+            for (int j=1; j<=n-i ; j++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=(2*i-1); j++ ){
+                System.out.print ("*");
+            }
+            System.out.println();
+        }
+    }}}
+
+    
+DIAMOND PATTERN IN SIMPLE BY USING Repeat ----------------------------  
+
+       int n = 4;
+
+for (int i = 1; i <= n; i++) {
+    System.out.print(" ".repeat(n - i));           //space
+    System.out.println("*".repeat(2 * i - 1));      //star print
+}
+
+for (int i = n - 1; i >= 1; i--) {
+    System.out.print(" ".repeat(n - i));
+    System.out.println("*".repeat(2 * i - 1));
+}
+
+
+    
+--------------------------next - in simple way------------------------------------ 
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+
+    int n = 5;
+for (int i=1; i<=n; i++){
+    System.out.println ("*".repeat (i));
+ 
+}
+for (int i=n-1; i>=1 ; i--){
+    System.out.println ("*".repeat (i));
+}
+
+
+    -------------------------diamond--------upper - lower pyramid -----------------
+
+
+
+
+    int n = 10;
+
+    for (int i = 1; i <= n; i++) {
+        System.out.print("".repeat(n - i));
+        System.out.println("*".repeat(2 * i - 1));
+    }
+    
+    for (int i = n - 1; i >= 1; i--) {
+        System.out.print("".repeat(n - i));
+        System.out.println("*".repeat(2 * i - 1));
+    }
+    }}
+    
+    
+
+    -------------------another method ---------------------
+    
+             int n = 10;
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) {
+            System.out.print(" ");
+        }
+        for (int k = 1; k <= i; k++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    for (int i = 9 ; i >= 1; i--) {
+        for (int j = n; j > i; j--) {
+            System.out.print(" ");
+        }
+        for (int k = 1; k <= i; k++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+        }}
+
+
+        
+    */
