67f5ce81099fe50663fe4a302491da67facf7e3e
Sahil19 <sahilkulria9988@gmail.com>
2025-04-13 18:27:37 +0530

    added

--- patterns.java
+++ patterns.java
@@ -248,5 +248,29 @@ for (int i=n-1; i>=1; i--){
      System.out.println("*".repeat(i));
 }}}
 
+---------------------Hollow box------------------
+
+    * * * * * * *
+    *           *
+    *           *
+    *           *
+    *           *
+    *           *
+    * * * * * * *
+    
+int n = 5;
+int m = 10;
+
+for (int i=1; i<=n ;i++){
+    for (int j=1; j<=m; j++){
+        if (i ==1 || i==n || j==1 || j==m)
+            System.out.print("*");
+        else
+            System.out.print(" ");
+        }
+                System.out.println();
+            }
+        }
+}
 
 */
